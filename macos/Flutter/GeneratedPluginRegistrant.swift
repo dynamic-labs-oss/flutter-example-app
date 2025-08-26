cda3177889cdb69cd413148f726f7b0a01563b94
Andreas Wiermann Casas <andreas.w.dev@gmail.com>
2025-08-26 17:59:13 -0300

    initial commit

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_web_auth_2
+import url_launcher_macos
+import webview_flutter_wkwebview
+import window_to_front
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
+  WindowToFrontPlugin.register(with: registry.registrar(forPlugin: "WindowToFrontPlugin"))
+}
